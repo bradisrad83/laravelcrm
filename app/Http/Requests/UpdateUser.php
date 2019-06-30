a4b06635090bda8422d22f45db5b68837ddfe516
Brad Goldsmith <bradgoldsmith@Brads-MBP.attlocal.net>
2019-06-30 19:36:53 -0400

    registered my polices and updated the store/update user requests

--- app/Http/Requests/UpdateUser.php
+++ app/Http/Requests/UpdateUser.php
@@ -13,7 +13,7 @@ class UpdateUser extends FormRequest
      */
     public function authorize()
     {
-        return false;
+       return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class UpdateUser extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'      => 'required|string',
+            'email'     => 'required|unique|email',
+            'role'      => 'required|numeric',
         ];
     }
 }
